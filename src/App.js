18ff5b4ca0c8fa8e10357f6882a9427e28c39a7e
Jani Harju <e2302304@edu.vamk.fi>
2024-09-24 06:05:42 +0300

    Tehtävä 7: Siirretty komponentit ali-hakemistoihin ja refaktoroitu nuolifunktioiksi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Expenses from './components/Expenses';
+import React from 'react';
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -12,7 +13,7 @@ function App() {
       id: 'e2', 
       title: 'New TV', 
       amount: 799.49, 
-      date: new Date("2023-01-12") 
+      date: new Date("2022-08-12") 
     },
     {
       id: 'e3',
@@ -24,7 +25,7 @@ function App() {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
-      date: new Date("2022-05-12"),
+      date: new Date("2023-01-09"),
     },
   ];
 
