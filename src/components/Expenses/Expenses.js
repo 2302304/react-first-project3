e241957ba42b799ef84319607456380fe2b0eac5
Jani Harju <e2302304@edu.vamk.fi>
2024-10-07 20:34:24 +0300

    Lisätty pylväsdiagrammi ja korjattu CSS-tyylejä

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 
 const Expenses = (props) => {
@@ -10,8 +11,7 @@ const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2023');
 
   const filterChangeHandler = (selectedYear) => {
-   
-    setFilteredYear(selectedYear);
+   setFilteredYear(selectedYear);
   };
 
   const filteredExpenses = props.items.filter((expense) => {
@@ -28,7 +28,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-  
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
 
     </Card>
